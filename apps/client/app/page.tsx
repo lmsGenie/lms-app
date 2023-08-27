78a81307b5fcafb2006eae7325729e9ebd8201ce
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2023-08-27 16:27:59 +0530

    feat: clean repo

--- apps/client/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@lms-genie/ui-components";
-
-export default async function Index() {
-  return (
-    <div className="text-lg text-blue-500">
-      <Button>Welcome to LMSGenie</Button>
-    </div>
-  );
-}
