c91de52655a9f7eee6d463dbb5de9ea2747cda72
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2023-08-27 17:34:38 +0530

    docs: test commit by adding comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen  flex-col items-center justify-between p-24">
-      Hello from LMSGENIE
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {/* Header */}
+      {/* Hero */}
+      Welcome to LMS-GENIE
     </main>
   );
 }
