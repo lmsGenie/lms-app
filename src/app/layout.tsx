cc5c94fae9e41d446e475faf3b60296376f4203e
sub1120 <subhamsahu1120@gmail.com>
2023-08-30 23:05:42 +0530

    feat(header.tsx): created header for page layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 import "./globals.css";
-import "./globals.css";
+
+import Header from "@/components/layout/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header></Header>
+        {children}
+      </body>
     </html>
   );
 }
---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,117 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+const Header = () => {
+  return (
+    <header className="sticky top-0">
+      <nav className="flex items-center justify-between bg-gray-900 px-5 text-gray-500 md:px-8 lg:px-10">
+        <ul className="flex  gap-4  md:gap-6 lg:gap-8 ">
+          <li>
+            <Link
+              className="hover:border-primary-500 active:border-primary-500 flex h-16 w-20 items-center justify-center border-t-2 border-gray-900 transition-colors ease-in-out hover:border-t-2 hover:text-white"
+              href="/"
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="hover:border-primary-500 active:border-primary-500 flex h-16 w-20 items-center justify-center  border-t-2 border-gray-900  transition-colors ease-in-out hover:border-t-2 hover:text-white"
+              href="/"
+            >
+              Courses
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="hover:border-primary-500 active:border-primary-500 flex h-16 w-20 items-center justify-center  border-t-2 border-gray-900  transition-colors ease-in-out hover:border-t-2 hover:text-white"
+              href="/"
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="hover:border-primary-500 active:border-primary-500 flex h-16 w-20 items-center justify-center  border-t-2 border-gray-900  transition-colors ease-in-out hover:border-t-2 hover:text-white"
+              href="/"
+            >
+              Contact
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="hover:border-primary-500 active:border-primary-500 flex h-16 w-48 items-center justify-center  border-t-2 border-gray-900  transition-colors ease-in-out hover:border-t-2 hover:text-white"
+              href="/"
+            >
+              Become an Instructor
+            </Link>
+          </li>
+        </ul>
+        <div className="flex gap-2">
+          <Select>
+            <SelectTrigger className="border-none bg-slate-900">
+              <SelectValue placeholder="USD" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="usd">USD</SelectItem>
+              <SelectItem value="rs">Rupees</SelectItem>
+            </SelectContent>
+          </Select>
+
+          <Select>
+            <SelectTrigger className="border-none bg-slate-900">
+              <SelectValue placeholder="English" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="english">English</SelectItem>
+              <SelectItem value="hindi">Hindi</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+      </nav>
+      <div className="flex h-16 items-center justify-between bg-white px-5 md:px-8 lg:px-10">
+        <div className="flex gap-4">
+          <div>
+            <Image
+              src="/lmsg.png"
+              alt="lmsg logo"
+              width={120}
+              height={80}
+            ></Image>
+          </div>
+
+          <Select>
+            <SelectTrigger className="w-44">
+              <SelectValue placeholder="Browse" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="browse">Browse</SelectItem>
+            </SelectContent>
+          </Select>
+
+          <Input
+            className="w-72"
+            type="text"
+            placeholder="What do you want learn..."
+          />
+        </div>
+        <div className="flex gap-4">
+          <Button>Create Account</Button>
+          <Button>Sign In</Button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
