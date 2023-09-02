218590436184a2fa86ca8fd1c1039eacf31d0f09
sub1120 <subhamsahu1120@gmail.com>
2023-09-02 13:05:44 +0530

    style(icon.tsx): setup icons using svg+sprite method, also added icon types for code suggessions"

--- src/components/toggle/ModeToggle.tsx
+++ src/components/toggle/ModeToggle.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import * as React from "react";
-import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
 import { useTheme } from "next-themes";
 
 import { Button } from "@/components/ui/button";
@@ -12,6 +11,8 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
+import Icon from "../icon/Icon";
+
 export function ModeToggle() {
   const { setTheme } = useTheme();
 
@@ -19,8 +20,14 @@ export function ModeToggle() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon">
-          <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <Icon
+            id="Sun"
+            className="absolute h-6 w-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+          />
+          <Icon
+            id="MoonStars"
+            className="absolute h-6 w-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+          />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
