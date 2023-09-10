ae1468b482fd0c536344b83b9352c7feb7d84161
sub1120 <subhamsahu1120@gmail.com>
2023-09-11 00:14:33 +0530

    feat(page.tsx): added hero section to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,37 @@
+import Image from "next/image";
 import Link from "next/link";
 import { categories } from "@/sampledata";
 
+import { Button } from "@/components/ui/button";
 import Icon from "@/components/icon/Icon";
+
+import bannerImage from "../assets/hero.png";
 
 export default function Home() {
   return (
     <main>
+      {/* hero section */}
+      <section className="mb-4 flex gap-6 bg-gray-50 py-5 pl-10 md:mb-6 md:gap-8 md:py-0 md:pl-16 lg:mb-8 lg:gap-10 lg:pl-20">
+        <div className="flex flex-col justify-center gap-3">
+          <h1 className="text-2xl font-semibold sm:text-3xl md:text-4xl lg:text-5xl">
+            Learn with expert anytime anywhere
+          </h1>
+          <p className="md:text-md text-sm lg:text-lg">
+            Our mision is to help people to find the best course online and
+            learn with expert anytime, anywhere.
+          </p>
+          <Button className="w-fit">Create Account</Button>
+        </div>
+
+        <Image
+          className="hidden md:block md:w-[700px] lg:w-[800px]"
+          src={bannerImage}
+          alt="banner image"
+        />
+      </section>
+
       {/* top categories section */}
-      <section className="my-4 mt-4 px-10 md:my-6 md:mt-6 md:px-16 lg:my-8 lg:mt-8 lg:px-20">
+      <section className="my-4 px-10 md:my-6 md:px-16 lg:my-8 lg:px-20">
         <h1 className="my-4 text-center text-lg font-semibold md:my-5 md:text-xl lg:my-6 lg:text-2xl">
           Browse Top Category
         </h1>
