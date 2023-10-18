22a890c9eb067789335e54eceb2a020556c1c65a
Vinay Pratap Singh <technicalharvi@gmail.com>
2023-10-18 15:42:52 +0530

    feat: breadcrumb

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Breadcrumb from "@/components/contact/Breadcrumb";
+
+const page = () => {
+  return (
+    <div>
+      {/* adding the breadcrumb */}
+      <Breadcrumb title="Contact" />
+    </div>
+  );
+};
+
+export default page;
---
+++ src/components/contact/Breadcrumb.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import React from "react";
+
+interface IProps {
+  title: string;
+}
+
+const Breadcrumb = ({ title }: IProps) => {
+  return (
+    <div className="space-y-2 bg-gray-50 py-10 text-center">
+      <h1 className="text-2xl font-semibold">{title}</h1>
+      <p className="text-sm">
+        <span className="text-gray-600">Home / </span>
+        {title}
+      </p>
+    </div>
+  );
+};
+
+export default Breadcrumb;
