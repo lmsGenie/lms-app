c270f0bd57383dc69197f5d1b8cdc3cba60d9bf1
Vinay Pratap Singh <technicalharvi@gmail.com>
2023-10-24 18:35:22 +0530

    fix: replaced form with shadcn form

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,39 +4,53 @@ import React from "react";
 import Image from "next/image";
 import mainImage from "@/assets/contactPage/main.png";
 import { branches } from "@/sampledata";
-import { SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
 
 import { IBranch } from "@/types/contact";
 import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 import Breadcrumb from "@/components/contact/Breadcrumb";
 import Icon from "@/components/icon/Icon";
 
-// form data
-type IFormData = {
-  firstName: string;
-  lastName: string;
-  email: string;
-  subject: string;
-  message: string;
-};
+const contactFormSchema = z.object({
+  firstName: z.string().min(3, {
+    message: "Please enter a valid first name",
+  }),
+  lastName: z.string().min(3, {
+    message: "Please enter a valid last name",
+  }),
+  email: z.string().email("Please enter a valid email"),
+  subject: z.string().min(5, "Please enter a valid subject"),
+  message: z.string().min(20, "Please enter a valid message"),
+});
 
 const page = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<IFormData>({
+  const form = useForm<z.infer<typeof contactFormSchema>>({
+    resolver: zodResolver(contactFormSchema),
     defaultValues: {
-      email: "",
       firstName: "",
       lastName: "",
+      email: "",
       message: "",
       subject: "",
     },
   });
 
   // handling the form submit
-  const onSubmit: SubmitHandler<IFormData> = (data) => console.log(data);
+  const onSubmit = (values: z.infer<typeof contactFormSchema>) => {
+    console.log(values);
+  };
 
   return (
     <div>
@@ -130,186 +144,109 @@ const page = () => {
           </div>
 
           {/* contact form */}
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="flex flex-col gap-5 bg-white p-2 dark:bg-gray-900 sm:p-5 lg:p-10">
-              {/* form header details */}
-              <div className="space-y-1">
-                <h1 className="text-lg font-semibold md:text-2xl">
-                  Get in touch
-                </h1>
-                <p className="text-sm text-gray-500">
-                  Feel free contact with us, we love to make new partners &
-                  friends
-                </p>
-              </div>
+          <div className="space-y-5 bg-white p-2 sm:p-5 lg:p-10">
+            {/* form header */}
+            <div className="space-y-1">
+              <h1 className="text-2xl font-medium">Get in touch</h1>
+              <p className="text-sm text-gray-500">
+                Feel free contact with us, we love to make new partners and
+                friends
+              </p>
+            </div>
 
-              <div className="flex flex-col gap-3">
-                {/* for name */}
-                <div className="flex flex-col gap-5 lg:flex-row">
-                  <label
-                    htmlFor="firstName"
-                    className="flex flex-col gap-1 text-sm font-medium"
-                  >
-                    First Name
-                    <input
-                      type="text"
-                      placeholder="First name ..."
-                      className={`rounded-md border-2 p-2 outline-none ${
-                        errors?.firstName
-                          ? "border-primary-500"
-                          : "border-gray-500"
-                      }`}
-                      {...register("firstName", {
-                        required: {
-                          value: true,
-                          message: "Please enter the first name",
-                        },
-                        minLength: {
-                          value: 3,
-                          message: "Please enter a valid first name",
-                        },
-                      })}
-                    />
-                    {/* for displaying error */}
-                    {errors?.firstName && (
-                      <p className="text-sm text-red-500">
-                        {errors?.firstName?.message}
-                      </p>
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-4"
+              >
+                <div className="flex flex-col items-center gap-5 lg:flex-row">
+                  {/* for first name */}
+                  <FormField
+                    control={form.control}
+                    name="firstName"
+                    render={({ field }) => (
+                      <FormItem className="w-full">
+                        <FormLabel>First Name</FormLabel>
+                        <FormControl>
+                          <Input placeholder="First name ..." {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
                     )}
-                  </label>
-                  <label
-                    htmlFor="lastName"
-                    className="flex flex-col gap-1 text-sm font-medium"
-                  >
-                    Last Name
-                    <input
-                      type="text"
-                      placeholder="Last name ..."
-                      className={`rounded-md border-2 p-2 outline-none ${
-                        errors?.lastName
-                          ? "border-primary-500"
-                          : "border-gray-500"
-                      }`}
-                      {...register("lastName", {
-                        required: {
-                          value: true,
-                          message: "Please enter the last name",
-                        },
-                        minLength: {
-                          value: 3,
-                          message: "Please enter a valid last name",
-                        },
-                      })}
-                    />
-                    {/* for displaying error */}
-                    {errors?.lastName && (
-                      <p className="text-sm text-red-500">
-                        {errors?.lastName?.message}
-                      </p>
+                  />
+
+                  {/* for last name */}
+                  <FormField
+                    control={form.control}
+                    name="lastName"
+                    render={({ field }) => (
+                      <FormItem className="w-full">
+                        <FormLabel>Last Name</FormLabel>
+                        <FormControl>
+                          <Input placeholder="Last name ..." {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
                     )}
-                  </label>
+                  />
                 </div>
 
                 {/* for email */}
-                <label
-                  htmlFor="email"
-                  className="flex flex-col gap-1 text-sm font-medium"
-                >
-                  Email
-                  <input
-                    type="email"
-                    placeholder="test@gmail.com"
-                    className={`rounded-md border-2 p-2 outline-none ${
-                      errors?.email ? "border-primary-500" : "border-gray-500"
-                    }`}
-                    {...register("email", {
-                      required: {
-                        value: true,
-                        message: "Please enter the email id",
-                      },
-                      pattern: {
-                        value: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
-                        message: "Please enter a valid email id",
-                      },
-                    })}
-                  />
-                  {/* for displaying error */}
-                  {errors?.email && (
-                    <p className="text-sm text-red-500">
-                      {errors?.email?.message}
-                    </p>
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input placeholder="test@gmail.com" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
                   )}
-                </label>
+                />
 
                 {/* for subject */}
-                <label
-                  htmlFor="subject"
-                  className="flex flex-col gap-1 text-sm font-medium"
-                >
-                  Subject
-                  <input
-                    type="text"
-                    placeholder="Message subject"
-                    className={`rounded-md border-2 p-2 outline-none ${
-                      errors?.subject ? "border-primary-500" : "border-gray-500"
-                    }`}
-                    {...register("subject", {
-                      required: {
-                        value: true,
-                        message: "Please enter the subject",
-                      },
-                      minLength: {
-                        value: 5,
-                        message: "Please enter a valid subject title",
-                      },
-                    })}
-                  />
-                  {/* for displaying error */}
-                  {errors?.subject && (
-                    <p className="text-sm text-red-500">
-                      {errors?.subject?.message}
-                    </p>
+                <FormField
+                  control={form.control}
+                  name="subject"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Subject</FormLabel>
+                      <FormControl>
+                        <Input placeholder="title ..." {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
                   )}
-                </label>
+                />
 
                 {/* for message */}
-                <label
-                  htmlFor="Message"
-                  className="flex flex-col gap-1 text-sm font-medium"
-                >
-                  Message
-                  <textarea
-                    placeholder="Your message ..."
-                    className={`h-32 resize-none rounded-md border-2 p-2 outline-none ${
-                      errors?.message ? "border-primary-500" : "border-gray-500"
-                    }`}
-                    {...register("message", {
-                      required: {
-                        value: true,
-                        message: "Please enter the message",
-                      },
-                      minLength: {
-                        value: 20,
-                        message: "Please enter a valid message",
-                      },
-                    })}
-                  />
-                  {/* for displaying error */}
-                  {errors?.message && (
-                    <p className="text-sm text-red-500">
-                      {errors?.message?.message}
-                    </p>
+                <FormField
+                  control={form.control}
+                  name="message"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Message</FormLabel>
+                      <FormControl>
+                        <Textarea
+                          placeholder="Write your message here ..."
+                          {...field}
+                          className="h-32 resize-none"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
                   )}
-                </label>
-              </div>
+                />
 
-              {/* submit button */}
-              <Button type="submit" className="lg:w-fit">
-                Send Message
-                <Icon id="PaperPlaneRight" className="ml-2 w-5" />
-              </Button>
-            </div>
-          </form>
+                <Button type="submit" className="w-full lg:w-fit">
+                  Send message
+                  <Icon id="PaperPlaneRight" className="ml-2 w-4" />
+                </Button>
+              </form>
+            </Form>
+          </div>
         </div>
       </section>
 
